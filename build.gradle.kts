c3c5dfd86b9a87d9e68a19a0d0afb33e78df2179
Roman Borodavkin <barad1tos@gmail.com>
2026-07-02 01:19:08 +0300

    build: update IntelliJ Platform Gradle Plugin
    - Upgrade org.jetbrains.intellij.platform to 2.17.0 after validating the prior LoggedErrorProcessor regression path
    
    - Remove the broad Dependabot ignore so future IntelliJ Platform Gradle Plugin updates are visible again
    
    Impact: Gradle plugin updates are unblocked while the affected test classpath remains valid

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 }
 
 plugins {
-    id("org.jetbrains.intellij.platform") version "2.13.1"
+    id("org.jetbrains.intellij.platform") version "2.17.0"
     kotlin("jvm") version "2.4.0"
     id("org.jetbrains.kotlinx.kover") version "0.9.8"
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
